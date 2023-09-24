b75cb9b32561accda8b8827e434ac012761d1e7e
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2023-09-24 13:59:09 +0700

    uji commit indexjs

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import UsersRoute from './routes/UsersRoute.js';
 import PendidikanRoute from './routes/PendidikanRoute.js';
 
 const app = express();
-//middleware
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(UsersRoute);
 app.use(PendidikanRoute);
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log('Server is running on port 5000');
 });
 

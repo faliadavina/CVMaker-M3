86782e9811336f1083573114b3ca5437ef093812
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2023-09-25 17:43:28 +0700

    edit database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('cvmaker_db', 'postgres', 'aininurulazizah', {
+const db = new Sequelize('cvmaker_db', 'postgres', '121212', {
   host: 'localhost',
   dialect: 'postgres',
 });

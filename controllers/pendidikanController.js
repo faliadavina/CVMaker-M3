5d8b0c4d1676a4b4082f1a8ba16caa57ced22ff6
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2023-09-25 19:00:10 +0700

    edit pendidikanController.js & PendidikanRoute.js

--- controllers/pendidikanController.js
+++ controllers/pendidikanController.js
@@ -21,3 +21,52 @@ export const insertPendidikan = async (req, res) => {
         res.json({ error: "Internal server error" });
     }
 };
+
+export const getAllPendidikan = async (req, res) => {
+    try {
+        const allPendidikan = await Pendidikan.findAll();
+
+        res.json(allPendidikan);
+    } catch (err) {
+        console.error(err);
+        res.json({ error: "Internal server error" });
+    }
+};
+
+export const getPendidikanById = async(req, res)=>{
+    try {
+    
+        const pendidikan = await Pendidikan.findOne({
+            where:{
+                id_pend : req.params.id_pend
+            }
+        })
+
+        res.json(pendidikan);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deletePendidikan = async(req, res)=>{
+    const pendidikan = await Pendidikan.findOne({
+        where:{
+            id_pend : req.params.id_pend
+        }
+    });
+    if(!pendidikan) {
+        res.json({error : "No Data Found"})
+    }
+
+    try {
+        await Pendidikan.destroy({
+            where:{
+                id_pend : req.params.id_pend
+            }
+        });
+        res.json({msg : "Deleted Successfuly"});
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- routes/PendidikanRoute.js
+++ routes/PendidikanRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
 import {
+    deletePendidikan,
+    getAllPendidikan,
+    getPendidikanById,
     insertPendidikan
 } from "../controllers/pendidikanController.js";
 
 const router = express.Router();
 router.post('/pendidikan', insertPendidikan);
+router.get('/pendidikan', getAllPendidikan)
+router.get('/pendidikan/:id_pend', getPendidikanById)
+router.delete('/pendidikan/:id_pend', deletePendidikan)
 
 export default router;
 

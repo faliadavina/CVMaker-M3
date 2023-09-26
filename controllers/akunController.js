436bcb6ef5cf60d8a241e6a9e073166dc36f3a1c
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2023-09-26 10:02:45 +0700

    edit-update-data

--- controllers/akunController.js
+++ controllers/akunController.js
@@ -1,14 +1,17 @@
 import AkunDB from "../models/akunModels.js";
+import md5 from "md5";
+//const md5 = require('md5');
 
-// Create user
+// Create Akun
 export const createAkun = async (req, res) => {
     try {
         const { username, email, password, role } = req.body;
+        const hashed_pass = md5(password);
 
         const newAkun = await AkunDB.create({
             username,
             email,
-            password,
+            password: hashed_pass,
             role
         });
 
@@ -47,36 +50,67 @@ export const getAkunById = async(req, res)=>{
     }
 }
 
-
-// Fungsi untuk memperbarui data pengguna berdasarkan user_id
 export const updateAkun = async (req, res) => {
     try {
-        const { id_akun } = req.params;
-        const { username, email, password, role } = req.body;
-
-        const akun = await AkunDB.findOne({
-            where: { id_akun }
-        });
-
-        if (!akun) {
-            return res.json({ error: 'Akun Pengguna tidak ditemukan' });
+      const { id_akun } = req.params;
+      const fieldsToUpdate = req.body;
+  
+      const akun = await AkunDB.findOne({
+        where: {
+          id_akun: id_akun
         }
-
-        // Memperbarui data pengguna
-        akun.username = username;
-        akun.email = email;
-        akun.password = password;
-        akun.role = role;
-
-        // Menyimpan perubahan ke database
-        await akun.save();
-
-        res.json(akun);
+      });
+  
+      if (!akun) {
+        return res.status(404).json({ error: 'Account not found' });
+      }
+  
+      // Update the specified fields
+      for (const field in fieldsToUpdate) {
+        if (Object.prototype.hasOwnProperty.call(fieldsToUpdate, field)) {
+          // Update each specified field
+          akun[field] = fieldsToUpdate[field];
+        }
+      }
+  
+      // Save the updated account to the database
+      await akun.save();
+      return res.json(akun);
     } catch (error) {
-        console.error(error.message);
-        res.json({ error: 'Internal server error' });
+      console.error('Error in updateAccount:', error.message);  // Log the error message
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
-};
+  };
+
+// Fungsi untuk memperbarui data pengguna berdasarkan user_id
+// export const updateAkun = async (req, res) => {
+//     try {
+//         const { id_akun } = req.params;
+//         const { username, email, password, role } = req.body;
+
+//         const akun = await AkunDB.findOne({
+//             where: { id_akun }
+//         });
+
+//         if (!akun) {
+//             return res.json({ error: 'Akun Pengguna tidak ditemukan' });
+//         }
+
+//         // Memperbarui data pengguna
+//         akun.username = username;
+//         akun.email = email;
+//         akun.password = password;
+//         akun.role = role;
+
+//         // Menyimpan perubahan ke database
+//         await akun.save();
+
+//         res.json(akun);
+//     } catch (error) {
+//         console.error(error.message);
+//         res.json({ error: 'Internal server error' });
+//     }
+// };
 
 
 export const deleteAkun = async(req, res)=>{

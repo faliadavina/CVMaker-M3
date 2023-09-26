c0e22e6ab7de408b1d8dc85620db6f2dc24e27ad
deasalmaisnaini <deaisnaini2@gmail.com>
2023-09-27 04:20:37 +0700

    add skill and login backend

---
+++ backend/controllers/akunController.js
@@ -0,0 +1,160 @@
+import AkunDB from "../models/akunModels.js";
+import bcrypt from 'bcryptjs';
+//const md5 = require('md5');
+
+// Create Akun
+export const createAkun = async (req, res) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    await AkunDB.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    res.status(201).json({
+      msg: 'Add user Berhasil',
+      akun: {
+        username,
+        email,
+        role
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+      
+}
+
+
+export const getAllAkun = async (req, res) => {
+    try {
+        const allAkun = await AkunDB.findAll();
+    
+        return res.status(200).json({
+          success: true,
+          allAkun,
+        });
+      } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({
+          error: error.message,
+        });
+      }
+};
+
+
+export const getAkunById = async (req, res) => {
+    try {
+        const akunById = await AkunDB.findOne({
+            where: {
+                id_akun: req.params.id_akun
+            }
+        });
+
+        if (!akunById) {
+            return res.status(404).json({
+                success: false,
+                message: 'Account not found for the given ID.'
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            akunById
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+    }
+};
+
+
+export const updateAkun = async (req, res) => {
+    try {
+      const { id_akun } = req.params;
+      const fieldsToUpdate = req.body;
+  
+      const akun = await AkunDB.findOne({
+        where: {
+          id_akun: id_akun
+        }
+      });
+  
+      if (!akun) {
+        return res.status(404).json({ error: 'Account not found' });
+      }
+  
+      // Update the specified fields
+      for (const field in fieldsToUpdate) {
+        if (Object.prototype.hasOwnProperty.call(fieldsToUpdate, field)) {
+          // Update each specified field
+          akun[field] = fieldsToUpdate[field];
+        }
+      }
+  
+      // Save the updated account to the database
+      await akun.save();
+      return res.json(akun);
+    } catch (error) {
+      console.error('Error in updateAccount:', error.message);  // Log the error message
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+
+// Fungsi untuk memperbarui data pengguna berdasarkan user_id
+// export const updateAkun = async (req, res) => {
+//     try {
+//         const { id_akun } = req.params;
+//         const { username, email, password, role } = req.body;
+
+//         const akun = await AkunDB.findOne({
+//             where: { id_akun }
+//         });
+
+//         if (!akun) {
+//             return res.json({ error: 'Akun Pengguna tidak ditemukan' });
+//         }
+
+//         // Memperbarui data pengguna
+//         akun.username = username;
+//         akun.email = email;
+//         akun.password = password;
+//         akun.role = role;
+
+//         // Menyimpan perubahan ke database
+//         await akun.save();
+
+//         res.json(akun);
+//     } catch (error) {
+//         console.error(error.message);
+//         res.json({ error: 'Internal server error' });
+//     }
+// };
+
+
+export const deleteAkun = async(req, res)=>{
+    const akun = await AkunDB.findOne({
+        where:{
+            id_akun : req.params.id_akun
+        }
+    });
+    if(!akun) {
+        res.json({error : "Akun Pengguna tidak ditemukan"})
+    }
+
+    try {
+        await AkunDB.destroy({
+            where:{
+                id_akun : req.params.id_akun
+            }
+        });
+        res.json({msg : "User Deleted Successfuly"});
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
---
+++ backend/middlewares/validationMiddleware.js
@@ -0,0 +1,15 @@
+import { validationResult } from 'express-validator';
+
+const validationMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  next();
+};
+
+export { validationMiddleware };

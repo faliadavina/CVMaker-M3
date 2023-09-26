6f28b3f7d8913b1af98c755f60f1deee9b65c808
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2023-09-26 10:00:41 +0700

    edit controllers/pendidikanController.js

--- controllers/pendidikanController.js
+++ controllers/pendidikanController.js
@@ -6,7 +6,7 @@ export const insertPendidikan = async (req, res) => {
         const { id_user, jenjang, nama_sekolah, jurusan, tahun_masuk, tahun_lulus } = req.body;
 
         // Membuat data pendidikan baru dalam objek
-        const newPendidikan =  await Pendidikan.create ({
+        const pendidikan =  await Pendidikan.create ({
             id_user,
             jenjang,
             nama_sekolah,
@@ -15,7 +15,7 @@ export const insertPendidikan = async (req, res) => {
             tahun_lulus
         });
 
-        res.json(newPendidikan);
+        res.json(pendidikan);
     } catch (err) {
         console.error(err);
         res.json({ error: "Internal server error" });
@@ -34,11 +34,12 @@ export const getAllPendidikan = async (req, res) => {
 };
 
 export const getPendidikanById = async(req, res)=>{
+    const { id_pend } = req.params;
+
     try {
-    
         const pendidikan = await Pendidikan.findOne({
             where:{
-                id_pend : req.params.id_pend
+                id_pend : id_pend
             }
         })
 
@@ -50,53 +51,55 @@ export const getPendidikanById = async(req, res)=>{
 
 export const updatePendidikan = async (req, res) => {
     try {
-        const { id_pend } = req.params;
-        const { jenjang, nama_sekolah, jurusan, tahun_masuk, tahun_lulus } = req.body;
-
-        const pendidikan = await Pendidikan.findOne({
-            where: { id_pend }
-        });
-
-        if (!pendidikan) {
-            return res.json({ error: 'Data tidak ditemukan' });
+      const { id_pend } = req.params;
+      const fieldsToUpdate = req.body;
+  
+      const pendidikan = await Pendidikan.findOne({
+        where: {
+            id_pend: id_pend
         }
+      });
+  
+      if (!pendidikan) {
+        return res.status(404).json({ error: 'Pendidikan not found' });
+      }
+  
+      // Update the specified fields
+      for (const field in fieldsToUpdate) {
+        if (Object.prototype.hasOwnProperty.call(fieldsToUpdate, field)) {
+          // Update each specified field
+          pendidikan[field] = fieldsToUpdate[field];
+        }
+      }
+  
+      // Save the updated skill to the database
+      await pendidikan.save();
+      return res.json(pendidikan);
 
-        // Memperbarui data pendidikan
-        pendidikan.jenjang = jenjang;
-        pendidikan.nama_sekolah = nama_sekolah;
-        pendidikan.jurusan = jurusan;
-        pendidikan.tahun_masuk = tahun_masuk;
-        pendidikan.tahun_lulus = tahun_lulus;
-
-        // Menyimpan perubahan ke database
-        await pendidikan.save();
-
-        res.json(pendidikan);
     } catch (error) {
-        console.error(error.message);
-        res.json({ error: 'Internal server error' });
+      console.error('Error in updatePendidikan:', error.message);  // Log the error message
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
-};
+  };
 
-export const deletePendidikan = async(req, res)=>{
+export const deletePendidikan = async (req, res) => {
+  const { id_pend } = req.params;
+
+  try {
     const pendidikan = await Pendidikan.findOne({
-        where:{
-            id_pend : req.params.id_pend
-        }
+      where: {
+        id_pend: id_pend,
+      }
     });
-    if(!pendidikan) {
-        res.json({error : "No Data Found"})
+
+    if (!pendidikan) {
+      return res.status(404).json({ error: 'Pendidikan not found' });
     }
 
-    try {
-        await Pendidikan.destroy({
-            where:{
-                id_pend : req.params.id_pend
-            }
-        });
-        res.json({msg : "Deleted Successfuly"});
-
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+    await pendidikan.destroy();
+    return res.json({ msg: 'Deleted Successfully' });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};

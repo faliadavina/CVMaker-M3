571daf617fe1c2b3a25f62aebcf0cec4f5e0cc23
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2023-09-26 10:33:52 +0700

    edit routes/PendidikanRoute.js

--- routes/PendidikanRoute.js
+++ routes/PendidikanRoute.js
@@ -12,7 +12,7 @@ router.post('/pendidikan', insertPendidikan);
 router.get('/pendidikan', getAllPendidikan);
 router.get('/pendidikan/:id_pend', getPendidikanById);
 router.patch('/pendidikan/:id_pend', updatePendidikan);
-router.delete('/pendidikan/:id_pend', deletePendidikan)
+router.delete('/pendidikan/:id_pend', deletePendidikan);
 
 export default router;
 

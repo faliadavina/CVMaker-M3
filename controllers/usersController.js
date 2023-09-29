cfb1747091d25c2688c6f51b8420a2a3894e3ed9
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2023-09-29 14:29:42 +0700

    merge-update-data

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -92,43 +92,6 @@ export const updateUser = async (req, res) => {
     }
   };
 
-
-// // Fungsi untuk memperbarui data pengguna berdasarkan user_id
-// export const updateUser = async (req, res) => {
-//     try {
-//         const { id_user } = req.params;
-//         const { nama, tempat_lahir, tanggal_lahir, alamat, status, telp, email, sosial_media, linkedin } = req.body;
-
-//         const user = await UserDB.findOne({
-//             where: { id_user }
-//         });
-
-//         if (!user) {
-//             return res.json({ error: 'Pengguna tidak ditemukan' });
-//         }
-
-//         // Memperbarui data pengguna
-//         user.nama = nama;
-//         user.tempat_lahir = tempat_lahir;
-//         user.tanggal_lahir = tanggal_lahir;
-//         user.alamat = alamat;
-//         user.status = status;
-//         user.telp = telp;
-//         user.email = email;
-//         user.sosial_media = sosial_media;
-//         user.linkedin = linkedin;
-
-//         // Menyimpan perubahan ke database
-//         await user.save();
-
-//         res.json(user);
-//     } catch (error) {
-//         console.error(error.message);
-//         res.json({ error: 'Internal server error' });
-//     }
-// };
-
-
 export const deleteUser = async(req, res)=>{
     const user = await UserDB.findOne({
         where:{

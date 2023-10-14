9e08b22804d9f610565eb2daf23c83c2c7f3c154
deasalmaisnaini <deaisnaini2@gmail.com>
2023-10-14 13:09:26 +0700

    memperbaiki hamburger menu

---
+++ frontend/src/pages/Hero.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import Typed from "typed.js";
+import axios from "axios"; // Import axios
+
+const Hero = () => {
+  const { user } = useSelector((state) => state.auth);
+  const username = user?.user?.username;
+  const id = user && user.user && user.user.id_akun;
+  const [skillNames, setSkillNames] = useState([
+    "Designer",
+    "Developer",
+    "Freelancer",
+    "Photographer"
+  ]);
+
+  useEffect(() => {
+    if (id) {
+      getSkills(); // Fetch skills when id is available
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (skillNames.length > 0) {
+      const options = {
+        strings: skillNames,
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+      };
+
+      const typed = new Typed(".typed", options);
+
+      return () => {
+        typed.destroy(); // Clean up the Typed instance to avoid memory leaks
+      };
+    }
+  }, [skillNames]);
+
+  const getSkills = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/skills/akun/${id}`);
+      const skills = response.data.skills;
+      const skillNames = skills.map((skill) => skill.nama_skill);
+      setSkillNames(skillNames);
+    } catch (error) {
+      console.error("Error fetching skills:", error);
+    }
+  };
+
+  return (
+    <div>
+      <Sidebar />
+      <section
+        id="hero"
+        className="d-flex flex-column justify-content-center align-items-center"
+      >
+        <div className="hero-container" data-aos="fade-in">
+          <h1>{username}</h1>
+          <p>
+            Saya Memiliki Skill {" "}
+            <span
+              className="typed"
+            ></span>
+          </p>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/pages/Sidebar.js
@@ -0,0 +1,192 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getMe, LogOut, reset } from "../features/authSlice";
+
+const Sidebar = () => {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+  const { isError } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/login");
+    }
+  }, [isError, navigate]);
+
+  const logout = () => {
+    dispatch(LogOut());
+    dispatch(reset());
+    navigate("/login");
+  };
+
+  const [activeMenuItem, setActiveMenuItem] = useState("My CV");
+  const [userData, setUserData] = useState({
+    username: "",
+    profileImage: "",
+  });
+
+  const { user } = useSelector((state) => state.auth);
+  const id_akun = user && user.user && user.user.id_akun;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/users/${id_akun}`
+        );
+        console.log(response.data);
+        const username = response.data.nama;
+        const profileImage = response.data.url;
+        console.log(username, profileImage);
+        setUserData({ username, profileImage });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id_akun]);
+
+  const handleMenuItemClick = (menuItem) => {
+    setActiveMenuItem(menuItem);
+  };
+
+  const mobileNavToggleHandler = () => {
+    document.body.classList.toggle("mobile-nav-active");
+    document.getElementById("navbar").classList.toggle("active");
+  };
+
+  return (
+    <div>
+      {/* Mobile nav toggle button */}
+      <i
+        className="bi bi-list mobile-nav-toggle d-xl-none"
+        onClick={mobileNavToggleHandler} // Handle klik pada ikon hamburger
+      ></i>
+
+      {/* ======= Header ======= */}
+      <header id="header">
+        <div className="d-flex flex-column">
+          <div className="profile">
+            <img
+              src={
+                userData.profileImage ||
+                "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"
+              }
+              alt=""
+              className="img-fluid rounded-circle"
+            />
+            <h1 className="text-light m-4">
+              <a href="index.html">{userData.username || ""}</a>
+            </h1>
+          </div>
+
+          <nav id="navbar" className="nav-menu navbar">
+            <ul>
+              <li>
+                <NavLink
+                  to="/hero"
+                  className={`nav-link ${
+                    activeMenuItem === "My CV" ? "scrollto active" : "scrollto"
+                  }`}
+                  onClick={() => handleMenuItemClick("My CV")}
+                >
+                  <i className="bx bx-home"></i> <span>My CV</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/data_diri"
+                  className={`nav-link ${
+                    activeMenuItem === "Edit Personal Data"
+                      ? "scrollto active"
+                      : "scrollto"
+                  }`}
+                  onClick={() => handleMenuItemClick("Edit Personal Data")}
+                >
+                  <i className="bx bx-user"></i> <span>Edit Personal Data</span>
+                </NavLink>
+              </li>
+              <li>
+                <a
+                  href="#resume"
+                  className={`nav-link ${
+                    activeMenuItem === "Edit Educational Data"
+                      ? "scrollto active"
+                      : "scrollto"
+                  }`}
+                  onClick={() => handleMenuItemClick("Edit Educational Data")}
+                >
+                  <i className="bx bx-file-blank"></i>{" "}
+                  <span>Edit Educational Data</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#portfolio"
+                  className={`nav-link ${
+                    activeMenuItem === "Edit Organizational Experience"
+                      ? "scrollto active"
+                      : "scrollto"
+                  }`}
+                  onClick={() =>
+                    handleMenuItemClick("Edit Organizational Experience")
+                  }
+                >
+                  <i className="bx bx-book-content"></i>{" "}
+                  <span>Edit Organizational Experience</span>
+                </a>
+              </li>
+              <li>
+                <NavLink
+                  to="/skills"
+                  className={`nav-link ${
+                    activeMenuItem === "Edit Skills"
+                      ? "scrollto active"
+                      : "scrollto"
+                  }`}
+                  onClick={() => handleMenuItemClick("Edit Skills")}
+                >
+                  <i className="bx bx-server"></i> <span>Edit Skills</span>
+                </NavLink>
+              </li>
+              <li>
+                <a
+                  href="#contact"
+                  className={`nav-link ${
+                    activeMenuItem === "Edit Portfolio"
+                      ? "scrollto active"
+                      : "scrollto"
+                  }`}
+                  onClick={() => handleMenuItemClick("Edit Portfolio")}
+                >
+                  <i className="bx bx-envelope"></i> <span>Edit Portfolio</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#logout"
+                  className="nav-link scrollto"
+                  onClick={logout}
+                >
+                  <i className="bx bx-log-in"></i> <span>Logout</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          {/* .nav-menu */}
+        </div>
+      </header>
+      {/* End Header */}
+    </div>
+  );
+};
+
+export default Sidebar;

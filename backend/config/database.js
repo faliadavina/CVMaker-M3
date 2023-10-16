f2aa3d00ff75ef8581d74131f2b03ae0edde07cc
Mentari Ayu Alysia Sudrajat <mentari.ayu.tif421@polban.ac.id>
2023-10-17 05:57:40 +0700

    organisasi dan landing page

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("cvmaker_db", "postgres", "121212", {
+const db = new Sequelize("cvmaker_db", "postgres", "Sun26042001", {
   host: "localhost",
   dialect: "postgres",
 });

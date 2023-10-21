7e79acea3fc3ac8be67711ec97578132364b9280
deasalmaisnaini <deaisnaini2@gmail.com>
2023-10-21 07:56:52 +0700

    update terbaru

--- frontend/src/components/My.js
+++ frontend/src/components/My.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Hero from "./Hero";
 
 const My = () => {
   const dispatch = useDispatch();
--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import Hero from "../components/Hero";
 
 const Layout = ({ children }) => {
   return (
     <div>
         <Sidebar />
+        <Hero />
         <main id = "main">
           {children}
         </main>
